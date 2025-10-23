f8be85ebb1f2112c47f8c8ed579142e61b1574b3
Bryamtf <u20312070@utp.edu.pe>
2025-10-22 19:19:35 -0500

    arreglando tremendo error de capa 8

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ import express from "express";
 import fs from "fs/promises";
 import dotenv from "dotenv";
 import cors from "cors";
+dotenv.config();
+
+const app = express();
 app.use(
   cors({
-    origin: "*", 
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-dotenv.config();
-
-const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 4000; 
